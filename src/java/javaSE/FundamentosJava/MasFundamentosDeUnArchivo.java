7a9f786b77cb258861d32eb5ec63669eb7f23a57
Yordan Aquiles Andres <yordanAquilesDev@gmail.com>
2026-05-07 20:23:16 +0000

    spuoh

---
+++ src/java/javaSE/FundamentosJava/MasFundamentosDeUnArchivo.java
@@ -0,0 +1,35 @@
+public class MasFundamentosDeUnArchivo {
+
+    // si muy bien se sabe que este es una clase java
+    // veremos un poco sobre las estructuras
+    // la condicional if simple tomo una decicion  
+    // muy usada para tomar deciciones en un programa
+    String pasword = "123456789";
+    String usuario = "user";
+    int edad;
+    boolean esmayor = (edad > 18);
+    boolean Autenticado=(pasword=="123456789" && usuario=="user");
+    if(Autenticado){
+        //tienes acceso 
+    }
+
+    if(esmayor)
+    {
+        // la variable si es mayor a 18
+
+    };if(edad>18)
+    {
+        // la variable si es mayor a 18
+
+    };if(true)
+    {
+        // esta condicion siempre se cumplira
+
+    };
+
+    if(false)
+    {
+        // esta condicion nunca se cumplira
+    }
+
+}
